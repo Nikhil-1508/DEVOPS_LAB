ef5fd10648879b80b63c9c7a2fdc44e5a3d4db5a
Nikhil R Yalawar <nikhilryalawar@gmail.com>
2025-04-02 15:01:01 +0530

    Update first.cpp

--- first.cpp
+++ first.cpp
@@ -1,6 +1,15 @@
 #include<iostream>
 using namespace std;
 int main(){
+  int n;
   cout<<"Hello World\n";
+  cout<<"Enter the value of n:\n";
+  cin>>n;
+  if(n%2==0){
+    cout<<"It is even";
+  }
+  else{
+    cout<<"It is odd";
+  }
   return 0;
 }
